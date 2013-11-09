b2035b68941a53d841958d97005082364b59e0bd
Hektor Wallin <hektorw@gmail.com>
2013-11-09 20:31:36 +0100

    Got grunt-less to work in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,5 +2,38 @@
 "use strict";
 
 module.exports = function(grunt) {
+  grunt.initConfig({
+    app: {
+      dev: 'dev',
+      dist: 'dist'
+    },
+    less: {
+      options: {
+        files: [
+        ]
+      },
+      development: {
+        files: {
+          '<%= app.dev %>/main.css': '<%= app.dev %>/styles/*.less',
+        }
+      },
+      dist: {
+        files: {
+          '<%= app.dist %>/main.css': '<%= app.dev %>/styles/*.less',
+        }
+      }
+    },
+    autoprefixer: {
 
+    }
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+  grunt.registerTask('default', [
+    'less:dist'
+    // 'autoprefixer'
+  ]);
 };
